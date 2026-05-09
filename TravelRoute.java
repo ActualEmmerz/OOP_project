fdae642c7f2fb381c5451aaee0f3ecca60194dc2
Emma <emuhruru@gmail.com>
2026-05-09 15:00:10 -0500

    Implement Builder pattern in TravelRoute class
    Refactored TravelRoute class to use a private constructor and added a Builder pattern for constructing TravelRoute instances.

--- TravelRoute.java
+++ TravelRoute.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 
 public class TravelRoute {
-    private ArrayList<Location> stops = new ArrayList<>();
+
+    private ArrayList<Location> stops;
+
+    private TravelRoute() {
+        stops = new ArrayList<>();
+    }
 
     public void addStop(Location location) {
         stops.add(location);
@@ -12,8 +17,27 @@ public class TravelRoute {
     }
 
     public void printRoute() {
-        for (Location loc: stops) {
+        for (Location loc : stops) {
             System.out.println(loc.getName());
+        }
+    }
+
+    // Builder Pattern
+    public static class Builder {
+
+        private TravelRoute route;
+
+        public Builder() {
+            route = new TravelRoute();
+        }
+
+        public Builder addStop(Location location) {
+            route.addStop(location);
+            return this;
+        }
+
+        public TravelRoute build() {
+            return route;
         }
     }
 }
