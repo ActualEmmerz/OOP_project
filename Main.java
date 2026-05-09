8506da3d3e176a2ee756a73a389eaec3e7d394f4
Emma <emuhruru@gmail.com>
2026-05-09 14:52:42 -0500

    Refactor TravelRoute initialization using Builder pattern

--- Main.java
+++ Main.java
@@ -1,9 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        TravelRoute route = new TravelRoute();
-        //test locations
-        route.addStop(new Location("Chicago"));
-        route.addStop(new Location("London"));
-        route.addStop(new Location("Paris"));
+
+        // Builder Pattern
+        TravelRoute route = new TravelRoute.Builder()
+                .addStop(new Location("Chicago"))
+                .addStop(new Location("London"))
+                .addStop(new Location("Paris"))
+                .addStop(new Location("Dubai"))
+                .build();
+
+        System.out.println("Travel Route:");
+        route.printRoute();
+
+        System.out.println();
+
+        // Factory Pattern
+        TransportationStrategy flight =
+                TransportationFactory.createTransportation("flight");
+
+        TransportationStrategy train =
+                TransportationFactory.createTransportation("train");
+
+        // Singleton Pattern
+        TravelSimulationEngine engine =
+                TravelSimulationEngine.getInstance();
+
+        engine.runSimulation(route, flight);
+
+        System.out.println();
+
+        engine.runSimulation(route, train);
     }
 }
